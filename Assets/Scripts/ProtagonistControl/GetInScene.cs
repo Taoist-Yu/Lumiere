4fc665eb8d6f206197902126ed4e216a4e811548
reska <juceqin@gmail.com>
2018-11-08 12:59:29 +0800

    添加人物动画

---
+++ Assets/Scripts/ProtagonistControl/GetInScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetInScene : GameBehaviour
+{
+	//int getInScenceTime = 0;
+	public static bool inScence = false;
+	public float speed = 2f;
+	[SerializeField]
+	float targetPositionX = 0;
+	[SerializeField]
+	float targetPositionY = 3;
+	[SerializeField]
+	float targetPositionZ = -15;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!inScence)
+		{
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetPositionX, targetPositionY, targetPositionZ), step);
+			if (transform.position.x == targetPositionX && transform.position.y == targetPositionY)
+			{
+				inScence = true;
+				//this.GetComponent<Camera>().orthographic = true;
+			}
+			/*if (transform.rotation.x > 0)
+			{
+				transform.rotation = Quaternion.AngleAxis(-0.05f, transform.right) * transform.rotation;
+			}*/
+		}
+	}
+}
