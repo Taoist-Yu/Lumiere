d265767054b8f13cb9eeb2877b1f58549ee26fc7
Taoist_Yu <294196463@qq.com>
2018-11-15 11:13:21 +0800

    重新提交

---
+++ Assets/Scripts/GameContent/Ray/RayOriginalLuncher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayOriginalLuncher : RayLuncher {
+
+	private void Awake()
+	{
+		RayLuncherAwake();
+	}
+
+	// Use this for initialization
+	void Start () {
+		RayLuncherStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RayLuncherUpdate();
+	}
+
+}
