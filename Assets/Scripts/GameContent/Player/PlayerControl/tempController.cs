8dd78ec2de6f91100eb823d0c8e0bcff68518bc0
Taoist_Yu <294196463@qq.com>
2018-12-30 18:10:26 +0800

    临时保存（修改人物）

--- Assets/Scripts/GameContent/Player/PlayerControl/tempController.cs
+++ Assets/Scripts/GameContent/Player/PlayerControl/tempController.cs
@@ -2,9 +2,113 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tempController : MonoBehaviour {
+public class tempController : MonoBehaviour
+{
+	int cot = 0;
+	RaycastHit2D[] leftCastHit, rightCastHit, bottomCastHit, bottomLeftCastHit, bottomRightCastHit;
+	#region 射线检测是否检测到了非触发器的碰撞体
+	bool haveLefeFence
+	{
+		get
+		{
+			if (leftCastHit == null)
+			{
+				return false;
+			}
+			else
+			{
+				bool flag = false;
+				foreach(RaycastHit2D hit in leftCastHit)
+				{
+					if (!hit.collider.isTrigger)
+						flag = true;
+				}
+				return flag;
+			}
+		}
+	}
+	bool haveRightFence
+	{
+		get
+		{
+			if (rightCastHit == null)
+			{
+				return false;
+			}
+			else
+			{
+				bool flag = false;
+				foreach (RaycastHit2D hit in rightCastHit)
+				{
+					if (!hit.collider.isTrigger)
+						flag = true;
+				}
+				return flag;
+			}
+		}
+	}
+	bool haveBottomFence
+	{
+		get
+		{
+			if (bottomCastHit == null)
+			{
+				return false;
+			}
+			else
+			{
+				bool flag = false;
+				foreach (RaycastHit2D hit in bottomCastHit)
+				{
+					if (!hit.collider.isTrigger)
+						flag = true;
+				}
+				return flag;
+			}
+		}
+	}
+	bool haveBottomLeftFence
+	{
+		get
+		{
+			if (bottomLeftCastHit == null)
+			{
+				return false;
+			}
+			else
+			{
+				bool flag = false;
+				foreach (RaycastHit2D hit in bottomLeftCastHit)
+				{
+					if (!hit.collider.isTrigger)
+						flag = true;
+				}
+				return flag;
+			}
+		}
+	}
+	bool haveBottomRightFence
+	{
+		get
+		{
+			if (bottomRightCastHit == null)
+			{
+				return false;
+			}
+			else
+			{
+				bool flag = false;
+				foreach (RaycastHit2D hit in bottomRightCastHit)
+				{
+					if (!hit.collider.isTrigger)
+						flag = true;
+				}
+				return flag;
+			}
+		}
+	}
+	#endregion
 
-	bool haveLeftFence, haveRightFence, haveBottomFence, haveBottomLeftFence, haveBottomRightFence;
 	bool onGround = true;
 	int pressJumpCount;
 	[SerializeField]
@@ -35,11 +139,11 @@ public class tempController : MonoBehaviour {
 
 	void LaunchRaycast()
 	{
-		haveLeftFence = Physics2D.Raycast(transform.position + new Vector3(0, -bottom, 0), new Vector3(-leftRange, 0, 0), new Vector3(-leftRange, 0, 0).magnitude);
-		haveRightFence = Physics2D.Raycast(transform.position + new Vector3(0, -bottom, 0), new Vector3(rightRange, 0, 0), new Vector3(rightRange, 0, 0).magnitude);
-		haveBottomFence = Physics2D.Raycast(transform.position, new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
-		haveBottomLeftFence = Physics2D.Raycast(transform.position + new Vector3(-bottomEdge, 0, 0), new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
-		haveBottomRightFence = Physics2D.Raycast(transform.position + new Vector3(bottomEdge, 0, 0), new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
+		leftCastHit = Physics2D.RaycastAll(transform.position + new Vector3(0, -bottom, 0), new Vector3(-leftRange, 0, 0), new Vector3(-leftRange, 0, 0).magnitude);
+		rightCastHit = Physics2D.RaycastAll(transform.position + new Vector3(0, -bottom, 0), new Vector3(rightRange, 0, 0), new Vector3(rightRange, 0, 0).magnitude);
+		bottomCastHit = Physics2D.RaycastAll(transform.position, new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
+		bottomLeftCastHit = Physics2D.RaycastAll(transform.position + new Vector3(-bottomEdge, 0, 0), new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
+		bottomRightCastHit = Physics2D.RaycastAll(transform.position + new Vector3(bottomEdge, 0, 0), new Vector3(0, -bottomRange, 0), new Vector3(0, -bottomRange, 0).magnitude);
 		//Debug.Log("RightFence:" + haveRightFence + "   LeftFence:" + haveLeftFence + "   BottomFence:" + haveBottomFence);
 	}
 
@@ -48,8 +152,7 @@ public class tempController : MonoBehaviour {
 		if (haveBottomFence)
 		{
 			onGround = true;
-			RaycastHit2D hit = new RaycastHit2D();
-			hit = Physics2D.Raycast(transform.position, new Vector3(0, -bottomRange, 0));
+			RaycastHit2D hit = bottomCastHit[bottomCastHit.Length - 1];
 			positionOfLand = hit.point.y;
 		}
 		else onGround = false;
@@ -62,7 +165,7 @@ public class tempController : MonoBehaviour {
 		{
 			this.transform.Translate(new Vector3(walkSpeed * Time.fixedDeltaTime, 0, 0));
 		}
-		else if (h < 0 && !haveLeftFence)
+		else if (h < 0 && !haveLefeFence)
 		{
 			this.transform.Translate(new Vector3(-walkSpeed * Time.fixedDeltaTime, 0, 0));
 		}
@@ -71,6 +174,10 @@ public class tempController : MonoBehaviour {
 	void PlayerJump()
 	{
 		bool pressJump = Input.GetButtonDown("Jump");
+		if(pressJump == true)
+		{
+			Debug.Log(cot++);
+		}
 		//Debug.Log(pressJumpCount);
 		if (onGround)
 		{
@@ -118,8 +225,11 @@ public class tempController : MonoBehaviour {
 							}
 							else
 							{
-								TranslatePlayer(G);
-								verticalVelocity -= Time.fixedDeltaTime * G;
+								if (!haveBottomLeftFence && !haveBottomRightFence)
+								{
+									TranslatePlayer(G);
+									verticalVelocity -= Time.fixedDeltaTime * G;
+								}
 							}
 							break;
 						}
