fbbd467ded02583aa4e8dca7846a17215178c688
Taoist_Yu <294196463@qq.com>
2019-03-04 20:38:18 +0800

    完成终点

---
+++ Assets/Scripts/GameContent/Effect/Floating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Floating : MonoBehaviour
+{
+
+	Vector3 initPos;
+
+	public float speed;
+	public float range;
+
+	private void Start()
+	{
+		initPos = transform.position;
+	}
+
+	private void Update()
+	{
+		transform.position = initPos + new Vector3(0, range * Mathf.Sin(speed * Time.time), 0);
+	}
+}
