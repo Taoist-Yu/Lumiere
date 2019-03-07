e17d18b12cb4733f8db393b4a1b3292da6b27d50
reska <juceqin@gmail.com>
2019-03-07 22:53:11 +0800

    小修复

--- Assets/Scripts/GameContent/Player/PlayerControl/PlayerFloat.cs
+++ Assets/Scripts/GameContent/Player/PlayerControl/PlayerFloat.cs
@@ -6,7 +6,7 @@ public class PlayerFloat : MonoBehaviour {
 
 	GameObject Players;
 	float radian = -1.6f;
-	float perRadian = 0.02f;
+	float perRadian = 0.01f;
 	float radius = 0.4f;
 	bool down = false;
 	Vector3 oldPos;
@@ -22,12 +22,13 @@ public class PlayerFloat : MonoBehaviour {
 	{
 		if (down && radian > 4.71f)
 		{
-			perRadian = 0.02f;
+			perRadian = 0.01f;
 			radius = 0.4f;
 			down = false;
 		}
 		radian += perRadian; 
 		float dy = Mathf.Cos(radian) * radius;
+		float dys = Mathf.Sin(radian) * radius;
 
 		Vector3 targetPosition;
 		if (dy > 0)
@@ -35,16 +36,23 @@ public class PlayerFloat : MonoBehaviour {
 			targetPosition = new Vector3(0, 2*radius, 0);
 		}
 		else targetPosition = new Vector3(0, 0, 0);
+		transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, (1-Mathf.Abs(dys)) * Time.fixedDeltaTime);
+		if (!down)
+		{
+			if (transform.localPosition.y >= 2 * radius)
+			{
+				radian = 1.57f;
+				targetPosition = new Vector3(0, 2 * radius - 0.01f, 0);
+				transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, (1 - Mathf.Abs(dys)) * Time.fixedDeltaTime);
 
-		if(transform.localPosition.y > 2 * radius)
-		{
-			transform.localPosition = new Vector3(0, 0.79f, 0);
+			}
+			else if (transform.localPosition.y <= 0)
+			{
+				radian = 4.71f;
+				targetPosition = new Vector3(0, 2 * radius + 0.01f, 0);
+				transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, Mathf.Abs(dys) * Time.fixedDeltaTime);
+			}
 		}
-		else if(transform.localPosition.y < 0)
-		{
-			transform.localPosition = new Vector3(0, 0.01f, 0);
-		}
-		transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, Mathf.Abs(dy) * Time.fixedDeltaTime);
 	}
 
 	public void GoUp()
